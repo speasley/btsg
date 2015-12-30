df7bb54a15d7d898f04fb91c72e6dd97e2a0e3c2
James Seigel <github@paddlethis.com>
2015-12-30 13:54:19 -0700

    Adding requests section to site

---
+++ data/requests.js
@@ -0,0 +1,9 @@
+requests = [
+  {
+    date: '2015-12-25',
+    time: null,
+    title: 'The name of that book!',
+    description: 'On Wednesday someone mentioned a book on the topic of hairbrained schemes to grow more hair or something similar.  Please response if you know the book!',
+    link: 'emailto:somedomain.ca'
+  }
+];
