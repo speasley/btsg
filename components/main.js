d99dd7ad1da8d4d1c73698c1f94fe2adadb455f6
James Seigel <github@paddlethis.com>
2015-12-30 13:53:28 -0700

    Added a request for information section to the page and the data file.

--- components/main.js
+++ components/main.js
@@ -17,7 +17,7 @@ class List extends React.Component {
 
   class ListEntry extends React.Component {
     render() {
-      let dateString = <span>[{this.props.date}]</span>;
+      let dateString = <span>[{this.props.date}]&nbsp;</span>;
       if(this.props.date === null) {
         dateString = <span></span>;
       }
@@ -35,13 +35,21 @@ class List extends React.Component {
             return (
               <div>
                 <div className="row">
-                  <div className="medium-12 columns">BTSG YYC</div>
+                  <div className="medium-12 columns">
+                    <h2>BTSG YYC</h2>
+                  </div>
                 </div>
                 <div className="row">
-                  <div className="medium-8 columns">Main Content</div>
-                  <div className="medium-4 columns">
+                  <div className="medium-7 columns">
+                    <p>This is the home for the YYC brain tumour support group, support site.  Our goal is to be a place to share resources that might be helpful to those attending support groups but just can't remember what book or website that someone has mentioned!  Happens to us pretty much everything we go!</p>
+                    <p>
+                    Please feel free to send us links, books, or other resources that you think might be helpful and we can try and get them up on the site.  If you are looking for a resource that was mentioned, send a request, we will publish it and hopefully someone remembers what it was.
+                    </p>
+                  </div>
+                  <div className="medium-5 columns">
                     <List items={this.props.events} listTitle={"Events"} classInfo={"callout secondary"}/>
                     <List items={books} listTitle={"Books"} classInfo={"callout secondary"}/>
+                    <List items={requests} listTitle={"Remember 4 Me"} classInfo={"callout secondary"}/>
                   </div>
                 </div>
               </div>
