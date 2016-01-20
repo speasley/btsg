b34fb106ac48da6fda434c7fa13e24d65fce5ffe
speasley <s@stephenpeasley.com>
2016-01-20 10:33:37 -0700

    Unify renders

--- components/main.js
+++ components/main.js
@@ -6,11 +6,11 @@ class List extends React.Component {
 
     return (
       <div className={this.props.classInfo}>
-      <h4>{this.props.listTitle}</h4>
-      <dl>
-        {items}
-      </dl>
-    </div>
+        <h4>{this.props.listTitle}</h4>
+        <dl>
+          {items}
+        </dl>
+      </div>
     );
   }
 }
@@ -33,15 +33,11 @@ class ListEntry extends React.Component {
 class NextMeeting extends React.Component {
   render() {
     return (
-      <div className="row">
-        <div className="small-12 columns text-center">
-          <div className="callout">
-            <h6>Next meeting:</h6>
-            <h5>Wednesday, {this.props.date} {this.props.time}</h5>
-            <p>Wellspring Calgary &mdash; 1404 Home Rd NW</p>
-            <p>{this.props.description}</p>
-          </div>
-        </div>
+      <div>
+        <h6>Next meeting:</h6>
+        <h5>Wednesday, {this.props.date} {this.props.time}</h5>
+        <p>Wellspring Calgary &mdash; 1404 Home Rd NW</p>
+        <p>{this.props.description}</p>
       </div>
     );
   }
@@ -50,20 +46,26 @@ class NextMeeting extends React.Component {
 class SupportApp extends React.Component {
   render() {
     return (
-      <div className="row">
-        <div className="small-12 medium-6 columns">
-          <List items={this.props.events} listTitle={"Events"} classInfo={"callout"}/>
+      <div>
+        <div className="row">
+          <div className="callout small-12 columns text-center">
+            <NextMeeting date={meeting.date} time={meeting.time} description={meeting.description}/>
+          </div>
         </div>
-        <div className="small-12 medium-6 columns">
-          <List items={books} listTitle={"Books"} classInfo={"callout"}/>
-        </div>
-        <div className="small-12 columns">
-          <List items={requests} listTitle={"Remember 4 Me"} classInfo={"callout"}/>
+        <div className="row">
+          <div className="small-12 medium-6 columns">
+            <List items={this.props.events} listTitle={"Events"} classInfo={"callout"}/>
+          </div>
+          <div className="small-12 medium-6 columns">
+            <List items={books} listTitle={"Books"} classInfo={"callout"}/>
+          </div>
+          <div className="small-12 columns">
+            <List items={requests} listTitle={"Remember 4 Me"} classInfo={"callout"}/>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-ReactDOM.render(<NextMeeting date={meeting.date} time={meeting.time} description={meeting.description} />, document.getElementById('next-meeting'));
-ReactDOM.render(<SupportApp events={events}/>, document.getElementById('react-here'));
+ReactDOM.render(<SupportApp events={events} meeting={meeting} />, document.getElementById('react-here'));
