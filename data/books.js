bd3445796771d1cf25f2d80bea296658de5acc2a
speasley <s@stephenpeasley.com>
2016-10-29 21:15:22 -0600

    Use unicode chars

--- data/books.js
+++ data/books.js
@@ -3,7 +3,7 @@ books = [
   date: null,
   time: null,
   title: 'The Tale of the Dueling Neurosurgeons: The History of the Human Brain as Revealed by True Stories of Trauma, Madness, and Recovery',
-  description: 'The author of the bestseller The Disappearing Spoon reveals the secret inner workings of the brain through strange but true stories.Early studies of the human brain used a simple method: wait for misfortune to strike -- strokes, seizures, infectious diseases, horrendous accidents -- and see how victims coped. In many cases their surviv&hellip;',
+  description: 'The author of the bestseller The Disappearing Spoon reveals the secret inner workings of the brain through strange but true stories.Early studies of the human brain used a simple method: wait for misfortune to strike \u2014 strokes, seizures, infectious diseases, horrendous accidents \u2014 and see how victims coped. In many cases their surviv\u2026',
   link: 'https://www.chapters.indigo.ca/en-ca/books/the-tale-of-the-dueling/9780316242257-item.html'
 }
 ]
