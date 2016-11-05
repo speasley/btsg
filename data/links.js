a05c973fbf8480b87f4d980a9aeb7cc40056f703
Stephen Peasley <speasley@users.noreply.github.com>
2016-11-04 18:08:08 -0600

    Add That Mitchell & Webb Look video

--- data/links.js
+++ data/links.js
@@ -2,6 +2,13 @@ links = [
 {
   date: null,
   time: null,
+  title: 'Brain Surgeon',
+  description: 'That Mitchell & Webb Look (2 min)',
+  link: 'https://www.youtube.com/watch?v=THNPmhBl-8I'
+},
+{
+  date: null,
+  time: null,
   title: 'My Brain Made Me Do It',
   description: 'The Nature of Things (45 min)',
   link: 'http://www.cbc.ca/natureofthings/episodes/my-brain-made-me-do-it'
