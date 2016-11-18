6dc41f64836a693d94b155e3789347da46fd3348
Stephen Peasley <speasley@users.noreply.github.com>
2016-11-18 11:30:05 -0700

    Update meeting.js

--- data/meeting.js
+++ data/meeting.js
@@ -1,5 +1,5 @@
 meeting = {
-  date: 'November 16, 2016',
+  date: 'December 21, 2016',
   time: '6:30pm',
-  description: 'Special Guest Speaker: Barb Gawley, Clinical Research Coordinator from TBCC – Neuro Oncology Group. Barb has done Clinical Research for 20 years. She has spent 13 years with the Nephrology group, and the last 7 years at TBCC. Barb joined our Neuro group 3 years ago. Please join us for an education in Clinical Trials and information about the Trials open for Neuro patients at the TBCC.'
+  description: 'Holiday potluck! Let us eat, drink and be merry!'
 };
