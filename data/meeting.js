145f7f7421842a4adabcb47d297c3c8e9e95413b
Stephen Peasley <speasley@users.noreply.github.com>
2017-01-25 10:34:32 -0700

    Add guest speaker details

--- data/meeting.js
+++ data/meeting.js
@@ -1,5 +1,5 @@
 meeting = {
   date: 'February 15, 2017',
   time: '6:30pm',
-  description: 'Regular meeting. Newcomers welcomed.'
+  description: 'Newcomers are welcome. Our guest speaker, Charlene Retzlaff, is a Social Worker and Coordinator at the Calgary Family Caregiver Centre'
 };
