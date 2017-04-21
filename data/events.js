f9ad6296819c2689c9ac74027d2473f4d4034528
Stephen Peasley <speasley@users.noreply.github.com>
2017-04-20 20:10:55 -0600

    Update events.js

--- data/events.js
+++ data/events.js
@@ -1,4 +1,11 @@
 events = [{
+  date: 'May 2, 2017',
+  time: null,
+  title: 'Calgary Tower Illumination',
+  description: 'The Calgary Tower will be illuminated to honour Brain Tumour Awareness Month.',
+  link: null
+},
+{
   date: 'Jun 3, 2017',
   time: null,
   title: 'Brain Tumour Walk',
