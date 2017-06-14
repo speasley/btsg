d84f926de3f747b263c22fee9e9991b7d85f3f03
Stephen Peasley <speasley@users.noreply.github.com>
2017-06-14 09:59:39 -0600

    Create meeting.js

--- data/meeting.js
+++ data/meeting.js
@@ -1,5 +1,5 @@
 meeting = {
   date: 'Wednesday, June 21',
   time: '6:30pm',
-  description: 'Regular meeting. Newcomers are welcomed.'
+  description: 'Summer pot luck! Please join us for some good conversation/friendship and yummy treats before we break for the summer. Email Lori to let her know if you are able to attend and what dish you are bringing. Meetings resume September 20.'
 };
