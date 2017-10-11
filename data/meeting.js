b62cfe15038c6673ea932c54560cf054d1c272bf
Stephen Peasley <speasley@users.noreply.github.com>
2017-10-11 10:48:02 -0700

    Update meeting.js

--- data/meeting.js
+++ data/meeting.js
@@ -1,5 +1,5 @@
 meeting = {
-  date: 'Wednesday, September 20',
+  date: 'Wednesday, October 18',
   time: '6:30pm',
   description: 'Regular meetings resume. Further details TBD. Newcomers welcomed.'
 };
